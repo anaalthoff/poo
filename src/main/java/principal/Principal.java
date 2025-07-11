dc104f6ba3a2c6bd3356373ca3b5d127d96fbc90
Nome do Usuário <email@gmail.com>
2025-07-10 22:12:37 -0300

    feat: substitui construtor com parâmetros por usa construtor vazio e setters para instanciar Retangulo

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -5,8 +5,21 @@ import classes.Retangulo;
 public class Principal {
 
     public static void main(String args[]) {
-        //Declara e instancia um objeto chamado ret da classe Retangulo
-        Retangulo ret = new Retangulo(5, 3);
+        //Declara e instancia um objeto chamado ret da classe Retangulo;;
+        // Retangulo ret = new Retangulo(5, 3);
+        //Recupera a base do objeto ret
+        // System.out.println(ret.getBase());
+        //Recupera a altura do objeto ret
+        // System.out.println(ret.getAltura());
+        
+        //Declara e instância um objeto chamado ret da classe Retangulo
+        Retangulo ret = new Retangulo();
+
+        //Modifica o atributo base do objeto ret
+        ret.setBase(2);
+        
+        //Modifica o atributo altura do objeto ret
+        ret.setAltura(3);
 
         //Recupera a base do objeto ret
         System.out.println(ret.getBase());
